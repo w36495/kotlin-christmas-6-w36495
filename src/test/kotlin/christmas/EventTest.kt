8b72a846c5bfb2718092be57a10fad82db978e9b
w36495 <w36495@naver.com>
2023-11-12 15:05:40 +0900

    style : 테스트 메서드명 수정

--- src/test/kotlin/christmas/EventTest.kt
+++ src/test/kotlin/christmas/EventTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 
 class EventTest {
     @Test
-    fun `크리스마스 디데이 이벤트 기간 외 평일 방문 한 경우, 디저트 메뉴 1개당 할인을 받는다`() {
+    fun `크리스마스 디데이 이벤트 기간 외 평일 방문한 경우, 디저트 메뉴 1개당 2_023원 할인을 받는다`() {
         val orders = mutableListOf(
             MenuDTO("티본스테이크", 1),
             MenuDTO("초코케이크", 1)
