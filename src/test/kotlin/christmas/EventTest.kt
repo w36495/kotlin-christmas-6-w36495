d361e52369cae13ef128638894037a5f146f7824
w36495 <w36495@naver.com>
2023-11-12 19:23:12 +0900

    test : 주말 할인만 적용된 기능 테스트 수정

--- src/test/kotlin/christmas/EventTest.kt
+++ src/test/kotlin/christmas/EventTest.kt
@@ -21,16 +21,15 @@ class EventTest {
 
     @Test
     fun `크리스마스 디데이 이벤트 기간 외 주말 방문한 경우, 메인 메뉴 1개당 2_023원 할인을 받는다`() {
-        val orders = mutableListOf(
-            MenuDTO("티본스테이크", 1),
-            MenuDTO("바비큐립", 2),
-            MenuDTO("아이스크림", 1)
+        val orders = mapOf(
+            "티본스테이크" to 1,
+            "바비큐립" to 2,
+            "아이스크림" to 1
         )
 
-        val discount = Event.Weekend.discount(orders)
-        val payment = Payment(orders)
-
-        val result = payment.getPreviousOrderPayment() - discount
+        val order = Order().createOrder(orders)
+        val discount = Event.Weekend.discount(order)
+        val result = Payment(order).getPreviousOrderPayment() - discount
 
         assertEquals(161_931, result)
     }
