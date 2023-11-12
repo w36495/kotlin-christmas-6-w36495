f093e8bf669e0bb2ee734a435246ed059c8b6f5b
w36495 <w36495@naver.com>
2023-11-12 21:24:42 +0900

    refactor : 할인 전 총 주문 금액 계산 기능 구현

--- src/main/kotlin/christmas/Payment.kt
+++ src/main/kotlin/christmas/Payment.kt
@@ -5,9 +5,6 @@ import christmas.dto.MenuDTO
 
 class Payment(private val orders: List<MenuDTO>) {
     fun getPreviousOrderPayment(): Int {
-        val orderFoodNames = orders.map { order -> order.foodName }
-        val orderMenuInfo = Menu.entries.filter { orderFoodNames.contains(it.foodName) }
-
-        return orderMenuInfo.sumOf { it.price }
+        return orders.sumOf { it.count * it.price }
     }
 }
