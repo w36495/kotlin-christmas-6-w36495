79a728aa32217b8ff7a80a75f12f8953cff241b8
w36495 <w36495@naver.com>
2023-11-12 21:18:38 +0900

    test : 평일 할인만 적용된 기능 테스트 수정

--- src/test/kotlin/christmas/EventTest.kt
+++ src/test/kotlin/christmas/EventTest.kt
@@ -1,19 +1,19 @@
 package christmas
 
-import christmas.dto.MenuDTO
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.assertEquals
 
 class EventTest {
     @Test
     fun `크리스마스 디데이 이벤트 기간 외 평일 방문한 경우, 디저트 메뉴 1개당 2_023원 할인을 받는다`() {
-        val orders = mutableListOf(
-            MenuDTO("티본스테이크", 1),
-            MenuDTO("초코케이크", 1)
+        val orders = mapOf(
+            "티본스테이크" to 1,
+            "초코케이크" to 1
         )
-        val discountAmount = Event.Weekday.discount(orders)
-        val payment = Payment(orders)
 
+        val order = Order().createOrder(orders)
+        val discountAmount = Event.Weekday.discount(order)
+        val payment = Payment(order)
         val result = payment.getPreviousOrderPayment() - discountAmount
 
         assertEquals(67_977, result)
