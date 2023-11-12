bf4a4e7c2444189446ec424b318715a988b414e9
w36495 <w36495@naver.com>
2023-11-12 22:12:04 +0900

    test : 크리스마스 디데이 할인 기능 테스트

--- src/test/kotlin/christmas/EventTest.kt
+++ src/test/kotlin/christmas/EventTest.kt
@@ -59,4 +59,23 @@ class EventTest {
 
         assertEquals(true, result)
     }
+
+    @Test
+    fun `크리스마스 디데이 기간 내 평일에 방문한 경우, 디저트 메뉴 1개당 2_023원의 할인과 크리스마스 할인을 받는다`() {
+        val visitDate = 4
+        val orderOfCustomer = mapOf(
+            "티본스테이크" to 1,
+            "타파스" to 2,
+            "초코케이크" to 2,
+            "제로콜라" to 1
+        )
+
+        val order = Order().createOrder(orderOfCustomer)
+        val payment = Payment(order)
+        val christmasDiscount = Event.Christmas.getDiscount(visitDate)
+        val weekdayDiscount = Event.Weekday.discount(order)
+        val result = payment.getPreviousOrderPayment() - (christmasDiscount + weekdayDiscount)
+
+        assertEquals(91_854, result)
+    }
 }
