c893558c9ddac25da2c01870383f1f7e8cf1c692
w36495 <w36495@naver.com>
2023-11-12 14:57:05 +0900

    feat : 평일 할인 기능 구현

---
+++ src/main/kotlin/christmas/Event.kt
@@ -0,0 +1,23 @@
+package christmas
+
+import christmas.dto.MenuDTO
+
+sealed class Event {
+    object Weekday {
+        private const val DISCOUNT: Int = 2_023
+
+        fun discount(orders: List<MenuDTO>): Int {
+            return DISCOUNT * countDessert(orders)
+        }
+
+        private fun countDessert(orders: List<MenuDTO>): Int {
+            var count = 0
+
+            if (hasDessert(orders)) count = orders.count { it.foodName == "초코케이크" || it.foodName == "아이스크림" }
+
+            return count
+        }
+
+        private fun hasDessert(orders: List<MenuDTO>): Boolean = orders.any { it.foodName == "초코케이크" || it.foodName == "아이스크림" }
+    }
+}
---
+++ src/main/kotlin/christmas/dto/MenuDTO.kt
@@ -0,0 +1,6 @@
+package christmas.dto
+
+data class MenuDTO(
+    val foodName: String,
+    val count: Int
+)
