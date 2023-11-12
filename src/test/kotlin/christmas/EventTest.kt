6f88c1d55681b07dd0be3ac7ab546596d1f67bfb
w36495 <w36495@naver.com>
2023-11-12 21:52:41 +0900

    test : 샴페인 증정 기능 테스트

--- src/test/kotlin/christmas/EventTest.kt
+++ src/test/kotlin/christmas/EventTest.kt
@@ -33,4 +33,30 @@ class EventTest {
 
         assertEquals(161_931, result)
     }
+
+    @Test
+    fun `할인 전 총 주문 금액이 12만 원 미만인 경우, 샴페인 1개 증정이 불가능하다`() {
+        val orderOfCustomer = mapOf(
+            "티본스테이크" to 1
+        )
+
+        val order = Order().createOrder(orderOfCustomer)
+        val preOrderPayment = Payment(order).getPreviousOrderPayment()
+        val result = Event.Present.canGetChampagne(preOrderPayment)
+
+        assertEquals(false, result)
+    }
+
+    @Test
+    fun `할인 전 총 주문 금액이 12만 원 이상인 경우, 샴페인 1개를 증정한다`() {
+        val orderOfCustomer = mapOf(
+            "티본스테이크" to 3
+        )
+
+        val order = Order().createOrder(orderOfCustomer)
+        val preOrderPayment = Payment(order).getPreviousOrderPayment()
+        val result = Event.Present.canGetChampagne(preOrderPayment)
+
+        assertEquals(true, result)
+    }
 }
